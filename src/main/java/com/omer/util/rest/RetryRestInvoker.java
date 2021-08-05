902e5af304e00d03caefeff8b0bc58993fefaeff
omerhashmininjago <hashmi.omer@gmail.com>
2021-08-06 02:12:45 +0530

    Rest Invoker initial commit

--- src/main/java/com/omer/util/rest/RetryRestInvoker.java
+++ src/main/java/com/omer/util/rest/RetryRestInvoker.java
@@ -4,14 +4,17 @@ import io.github.resilience4j.core.IntervalFunction;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
+import io.vavr.API;
 import io.vavr.control.Try;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.function.Supplier;
 
@@ -34,13 +37,16 @@ public interface RetryRestInvoker<T> extends RestInvoker<T> {
     @Component
     class RetryUtil {
 
+        private static final Logger LOG = LoggerFactory.getLogger(RetryUtil.class);
+
         @Value("${maxAttempts: 3}")
         private int maxAttempts;
         @Value("${intervalFunction: 500}")
         private Long intervalFunction;
-        @Value("${waitDuration: 30}")
+        @Value("${waitDuration: 5}")
         private Long waitDuration;
 
+        private static final ThreadLocal<Integer> API_INVOCATION_COUNT = new ThreadLocal<>();
         private static Retry RETRY;
 
         @PostConstruct
@@ -50,19 +56,24 @@ public interface RetryRestInvoker<T> extends RestInvoker<T> {
                             .intervalFunction(IntervalFunction.of(this.intervalFunction))
                             .maxAttempts(this.maxAttempts)
                             .waitDuration(Duration.ofSeconds(this.waitDuration))
-                            .retryExceptions(IOException.class)
+                            .retryExceptions(ResourceAccessException.class)
                     .build()).retry("retry");
         }
 
         public static <T> ResponseEntity<T> invoke(@NonNull String url, @NonNull HttpMethod httpMethod, @NonNull HttpEntity httpEntity, @NonNull Class classReference) {
-            Supplier<ResponseEntity> decoratedSupplier = Retry.decorateSupplier(RETRY, invokeEndPoint(url, httpMethod, httpEntity, classReference));
+            API_INVOCATION_COUNT.set(0);
+            Supplier<ResponseEntity> decoratedSupplier = Retry.decorateSupplier(RETRY, () -> invokeEndPoint(url, httpMethod, httpEntity, classReference));
 
-            return Try.ofSupplier(decoratedSupplier)
+            ResponseEntity responseEntity = Try.ofSupplier(decoratedSupplier)
                     .recover(throwable -> ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()).get();
+            API_INVOCATION_COUNT.remove();
+            return responseEntity;
         }
 
-        private static Supplier<ResponseEntity> invokeEndPoint(@NonNull String url, @NonNull HttpMethod httpMethod, @NonNull HttpEntity httpEntity, @NonNull Class classReference) {
-            return () -> REST_TEMPLATE.exchange(url, httpMethod, httpEntity, classReference);
+        private static ResponseEntity invokeEndPoint(@NonNull String url, @NonNull HttpMethod httpMethod, @NonNull HttpEntity httpEntity, @NonNull Class classReference) {
+            API_INVOCATION_COUNT.set(API_INVOCATION_COUNT.get() + 1);
+            LOG.debug("API Invocation Count : {}", API_INVOCATION_COUNT.get());
+            return REST_TEMPLATE.exchange(url, httpMethod, httpEntity, classReference);
         }
     }
 }
